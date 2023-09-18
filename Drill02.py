0377fa63a6660dc45d6fb9e6cf5a98b791a3222e
TUJungMin <paulkjm92@gmail.com>
2023-09-18 22:12:47 +0900

    Drill002제출(2020180002_곽정민)

--- Drill02.py
+++ Drill02.py
@@ -6,7 +6,7 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 x = 0
 y = 90
-angle = 0
+angle = -90
 center_x = 400
 center_y = 345
 
@@ -15,45 +15,45 @@ while True:
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        x += 5
+        x += 10
         delay(0.01)
 
     while (y < 600):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        y += 5
+        y += 10
         delay(0.01)
 
     while (x > 0):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        x -= 5
+        x -= 10
         delay(0.01)
 
     while (y > 90):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        y -= 5
+        y -= 10
         delay(0.01)
 
     while (x < 400):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        x += 5
+        x += 10
         delay(0.01)
 
-    while (angle < 360):
+    while (angle < 270):
         clear_canvas_now()
         grass.draw_now(400, 30)
         x = 400 + math.cos(math.radians(angle)) * 255
         y = 355 + math.sin(math.radians(angle)) * 255
         character.draw_now(x, y)
-        angle += 5
+        angle += 2
         delay(0.01)
-    angle = 0  # 들여쓰기가 올바르게 수정되었습니다.
+    angle = -90  # 들여쓰기가 올바르게 수정되었습니다.
 
 close_canvas()
